e7601bed9b018ea306e0b35af3e1502d0bbbe2bc
Nathan Robinson <nrobinson2000@me.com>
2018-11-14 23:27:42 -0500

    at 23:27 of 2018-11-14

--- python/main.py
+++ python/main.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 
+# inputs
+# Serial port
+# number of readings
+
 from __future__ import print_function
 import serial
 import sys
@@ -14,19 +18,30 @@ except IndexError:
     print()
     print("Please provide a serial port!")
     print("Example: ")
-    print(sys.argv[0] + " /dev/ttyACM0")
+    print(sys.argv[0] + " /dev/ttyACM0 10")
     print()
 
 except serial.serialutil.SerialException:
     print("Could not open serial port: " + sys.argv[1])
 
 else:
-    filename = "readings-" + str(time.time()) +".csv"
-    f = open(filename, "w")
-    f.write(header)
-    for i in range(0, 10):
-        line = ser.readline()
-        f.write(line)
-        print(line, end='')
-    ser.close()
-    f.close()
+
+    try:
+        count = sys.argv[2];
+
+    except IndexError:
+        print()
+        print("You must specify how many readings you want!")
+        print("Example: ")
+        print(sys.argv[0] + " /dev/ttyACM0 10")
+
+    else:
+        filename = "readings-" + str(time.time()) +".csv"
+        f = open(filename, "w")
+        f.write(header)
+        for i in range(0, int(count)):
+            line = ser.readline()
+            f.write(line)
+            print(line, end='')
+        ser.close()
+        f.close()
